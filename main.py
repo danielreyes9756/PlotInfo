bf3fafd8421d4b3e8b2958aaadf885d2af2456c2
Daniel Reyes <danielreyes9756@gmail.com>
2024-11-09 23:39:00 +0000

    adapt code to use continents instead of countries

--- main.py
+++ main.py
@@ -4,6 +4,49 @@ import pandas as pd
 import plotly.express as px
 
 
+countries_by_continent = {
+        "Africa": [
+            "Angola", "Burundi", "Benin", "Burkina Faso", "Botswana", "Cameroon", "Congo", "Cote d'Ivoire", "Comoros",
+            "Cape Verde", "Djibouti", "Eritrea", "Ethiopia", "Gabon", "Ghana", "Guinea", "Gambia", "Guinea-Bissau",
+            "Equatorial Guinea", "Kenya", "Liberia", "Libya", "Lesotho", "Madagascar", "Malawi", "Mali", "Mauritania",
+            "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Senegal", "Sierra Leone",
+            "Somalia", "South Sudan", "Sao Tome and Principe", "Togo", "Uganda", "Zambia", "Zimbabwe", "Sudan"
+            "Central African Republic", "Chad", "Egypt ", "Eswatini", "Seychelles", "South Africa", "Tanzania",
+            "Tunisia", "Algeria"
+        ],
+        "Asia": [
+            "Afghanistan", "Armenia", "Azerbaijan", "Bahrain", "Bangladesh", "Bhutan", "Brunei", "Cambodia", "China",
+            "Cyprus", "Georgia", "India", "Indonesia", "Iran", "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan",
+            "Kyrgyzstan", "Lebanon", "Lao", "Malaysia", "Maldives", "Myanmar", "Mongolia", "Nepal", "North Korea",
+            "Oman", "Pakistan", "Palestine", "Philippines", "Qatar", "Saudi Arabia", "Sri Lanka", "Syria", "Tajikistan",
+            "Turkmenistan", "Thailand", "Timor-Leste", "Turkey", "United Arab Emirates", "Uzbekistan", "Vietnam",
+            "Yemen", "Kuwait", "Laos", "Singapore", "South Korea", "Taiwan"
+        ],
+        "Europe": [
+            "Albania", "Andorra", "Austria", "Belgium", "Bulgaria", "Belarus", "Bosnia and Herzegovina", "Croatia",
+            "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Switzerland",
+            "Iceland", "Ireland", "Italy", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Monaco",
+            "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Norway", "Poland", "Portugal", "Romania",
+            "Russia", "San Marino", "Serbia", "Slovak Republic", "Slovenia", "Spain", "Sweden", "Ukraine",
+            "UK"
+        ],
+        "North America": [
+            "Canada", "United States", "Mexico", "Cuba", "Guatemala", "Honduras", "Jamaica", "Panama", "Dominica",
+            "Haiti", "Dominican Republic", "Barbados", "Belize", "Saint Kitts and Nevis", "Saint Lucia", "Grenada",
+            "Trinidad and Tobago", "Antigua and Barbuda", "Bahamas ", "Costa Rica", "El Salvador",
+            "Nicaragua",
+        ],
+        "South America": [
+            "Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname",
+            "Uruguay", "Venezuela"
+        ],
+        "Oceania": [
+            "Australia", "New Zealand", "Fiji", "Kiribati", "Marshall Islands", "Micronesia", "Palau", "Samoa",
+            "Solomon Islands", "Tonga", "Tuvalu", "Vanuatu", "Nauru", "Papua New Guinea"
+        ]
+    }
+
+
 def load_and_clean_data(file_path, dropna_columns):
     """
     Loads a CSV file and cleans NaN values based on specified columns.
@@ -29,6 +72,26 @@ def filter_top_countries(df, country_column, top_n=10):
     return df[df[country_column].isin(top_countries)]
 
 
+def add_continent_column(df, countries_by_continent):
+    """
+    Adds a 'continent' column to the dataframe by mapping countries to their continents.
+
+    :param df: DataFrame with a column 'Entity' (country names).
+    :param countries_by_continent: Dictionary with continents as keys and a list of countries as values.
+    :return: DataFrame with the added 'continent' column.
+    """
+    # Map countries by continents
+    country_to_continent = {}
+    for continent, countries in countries_by_continent.items():
+        for country in countries:
+            country_to_continent[country] = continent
+
+    # Replace 'country' column with 'continent' column
+    df['continent'] = df['Entity'].map(country_to_continent)
+
+    return df
+
+
 def plot_scatter():
     """
     Plots and saves as HTML a scatter (interactive)
@@ -41,23 +104,29 @@ def plot_scatter():
         ['Year', 'Primary energy consumption per capita (kWh/person)']
     )
 
-    # Select a few countries for a better visualization
-    df_filtered = filter_top_countries(df_cleaned, 'Entity')
+    # Map countries by continents
+    df_cleaned = add_continent_column(df_cleaned, countries_by_continent)
 
-    # Create a scatter plot
+    # Group year and continent to calculate the average energy consumption by continent and year
+    df_avg_consumption_by_continent_per_year =\
+        df_cleaned.groupby(
+            ['Year', 'continent'],
+            as_index=False)['Primary energy consumption per capita (kWh/person)'].mean()
+
+    # Create a scatter plot for average consumption per continent
     fig = px.scatter(
-        df_filtered,
+        df_avg_consumption_by_continent_per_year,
         x='Year',
         y='Primary energy consumption per capita (kWh/person)',
-        color='Entity',
-        title='Primary Energy Consumption per Capita Over Time',
+        color='continent',
+        title='Average Primary Energy Consumption per Capita by Continent Over Time',
         labels={
             'Year': 'Year',
             'Primary energy consumption per capita (kWh/person)': 'Energy Consumption (kWh/person)',
-            'Entity': 'Country'
+            'continent': 'Continent'
         },
         template='plotly_white',
-        color_discrete_sequence=px.colors.qualitative.Dark24
+        color_discrete_sequence=px.colors.qualitative.Set1
     )
 
     # Update traces
@@ -82,19 +151,20 @@ def plot_sunburst():
     # Add a new column for grouping by decade
     df_cleaned['decade'] = (df_cleaned['Year'] // 10) * 10
 
-    # Select a few countries for a better visualization
-    df_filtered = filter_top_countries(df_cleaned, 'Entity')
+    # Map countries by continents
+    df_cleaned = add_continent_column(df_cleaned, countries_by_continent)
 
-    # Group the dataframe by 'decade', 'Entity' and 'Primary energy consumption per capita (kWh/person)'
-    df_grouped = df_filtered.groupby(
-        ['decade', 'Entity']
-    )['Primary energy consumption per capita (kWh/person)'].mean().reset_index()
+    # Group year and continent to calculate the average energy consumption by continent and year
+    df_avg_consumption_by_continent_per_decade =\
+        df_cleaned.groupby(
+            ['decade', 'continent'],
+            as_index=False)['Primary energy consumption per capita (kWh/person)'].mean()
 
     # Create the Sunburst plot
-    fig = px.sunburst(df_grouped,
-                      path=['decade', 'Entity'],
+    fig = px.sunburst(df_avg_consumption_by_continent_per_decade,
+                      path=['decade', 'continent'],
                       values='Primary energy consumption per capita (kWh/person)',
-                      title="Primary Energy Consumption per Capita by Decade and Country",
+                      title='Average Primary Energy Consumption per Capita by Continent Over Time (Decade)',
                       color='Primary energy consumption per capita (kWh/person)',
                       template='plotly_white')
 
@@ -111,33 +181,48 @@ def plot_ridgeline():
     # Load and clean dataset
     df_cleaned = load_and_clean_data('./datasets/lex.csv', ['country'])
 
-    # Select 50 countries for a better visualization
-    df_filtered = filter_top_countries(df_cleaned, 'country', top_n=50)
+    # Map countries by continents
+    country_to_continent = {}
+    for continent, countries in countries_by_continent.items():
+        for country in countries:
+            country_to_continent[country] = continent
 
-    # Melt df
-    df_melted = df_filtered.melt(id_vars='country', var_name='year', value_name='lifeExp')
+    # Replace 'country' column with 'continent' column
+    df_cleaned['continent'] = df_cleaned['country'].map(country_to_continent)
 
-    # Create a Ridgeline plot
+    # Remove the 'country' column as we no longer need it
+    df_cleaned = df_cleaned.drop(columns=['country'])
+
+    # Melt df (changing 'country' to 'continent')
+    df_melted = df_cleaned.melt(id_vars='continent', var_name='year', value_name='lifeExp')
+
+    # Convert 'lifeExp' to numeric, coercing errors to NaN
+    df_melted['lifeExp'] = pd.to_numeric(df_melted['lifeExp'], errors='coerce')
+
+    # Calculate the average life expectancy per continent and year
+    df_avg_life_exp = df_melted.groupby(['continent', 'year'], as_index=False)['lifeExp'].mean()
+
+    # Create a Ridgeline plot using continents
     joyplot(
-        data=df_melted,
-        by='country',
+        data=df_avg_life_exp,
+        by='continent',
         column='lifeExp',
-        title="Life Expectancy Distribution by Country",
+        title="Life Expectancy Distribution by Continent",
         colormap=plt.cm.viridis,
         figsize=(17, 9),
-        x_range=(df_melted['lifeExp'].min(), df_melted['lifeExp'].max()),
+        x_range=(df_avg_life_exp['lifeExp'].min(), df_avg_life_exp['lifeExp'].max()),
         fade=True,
         grid=True,
     )
 
     plt.xlabel("Life Expectancy")
-    plt.ylabel("Country")
+    plt.ylabel("Continent")
     plt.savefig("ridgeline.png")
     plt.close()
 
-    # Save as HTML (rigid didn't find a proper way)
+    # Save as HTML
     with open("views/ridgeline.html", "w") as f:
-        f.write('<html><body><img src="../ridgeline.png" alt="Ridgeline Plot"></body></html>')
+        f.write('<html><body><img src="../ridgeline.png" alt="Ridgeline Plot"><br></body></html>')
 
 
 if __name__ == '__main__':
